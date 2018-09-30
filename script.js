acaa85b1bdff9e6c1a32ac3dbf6b9df3a32adfde
mkw98 <40563214+mkw98@users.noreply.github.com>
2018-09-30 12:44:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 $(function(){
 	//this code will execute after the DOM is loaded
 	var carouselList = $('#carousel ul');
-	var firstItem = carouselList.find('li:first');
-    var lastItem = carouselList.find('li:last');
 
-
-	/*function moveCarousel () {
-		var play = setInterval(changeSlide, 3000);
-	}
-
-	moveCarousel ();
-
-	/*setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide */
 
 	function changeSlide () {
-		carouselList.animate({'marginLeft': '-400px'}, 500, 'swing', moveFirstSlideAfterLast);
+		carouselList.animate({'marginLeft': '-400px'}, 500, moveFirstSlideAfterLast);
 
 	}
 	function moveFirstSlideAfterLast () {
 		var firstItem = carouselList.find('li:first');
-    	var lastItem = carouselList.find('li:last');
-    	lastItem.after(firstItem);
+    		var lastItem = carouselList.find('li:last');
+    		lastItem.after(firstItem);
    		carouselList.css({marginLeft:0});
 	}
 		setInterval(changeSlide, 3000);
